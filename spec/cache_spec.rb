bc366ff8804a82b8c2e5f6a4d9721d3dc8dcd9b1
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-07 15:48:22 +0100

    cache specs

--- spec/cache_spec.rb
+++ spec/cache_spec.rb
@@ -1,6 +1,17 @@
 require 'spec_helper'
 
 describe GH::Cache do
-  it 'send HTTP requests for uncached resources'
-  it 'uses the cache for subsequent requests'
+  before { subject.backend = GH::MockBackend.new }
+
+  it 'send HTTP requests for uncached resources' do
+    subject['users/rkh']['name'].should be == "Konstantin Haase"
+    requests.count.should be == 1
+  end
+
+  it 'uses the cache for subsequent requests' do
+    subject['users/rkh']['name'].should be == "Konstantin Haase"
+    subject['users/svenfuchs']['name'].should be == "Sven Fuchs"
+    subject['users/rkh']['name'].should be == "Konstantin Haase"
+    requests.count.should be == 2
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,53 @@
 require 'gh'
+require 'webmock/rspec'
+require 'yaml'
+require 'fileutils'
+
+module GH
+  module TestHelpers
+    def backend
+      subject.backend
+    end
+
+    def requests
+      backend.requests
+    end
+  end
+
+  class MockBackend < Wrapper
+    attr_accessor :data, :requests
+
+    def initialize(*)
+      @data, @requests = {}, []
+      super
+    end
+
+    def [](key)
+      key  = path_for(key)
+      file = File.expand_path("../payloads/#{key}.yml", __FILE__)
+      @requests << key
+
+      unless File.exist? file
+        res = allow_http { super }
+        FileUtils.mkdir_p File.dirname(file)
+        File.write file, [res.headers, res.body].to_yaml
+      end
+
+      @data[key] ||= Response.new(*YAML.load_file(file))
+    end
+
+    private
+
+    def allow_http
+      raise Faraday::Error::ResourceNotFound if ENV['CI']
+      WebMock.allow_net_connect!
+      yield
+    ensure
+      WebMock.disable_net_connect!
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.include GH::TestHelpers
+end
