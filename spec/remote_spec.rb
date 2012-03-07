a955e9e35e17fdf7bdb0be11f8bc792bf5028cdf
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-07 15:48:03 +0100

    remote specs

--- spec/remote_spec.rb
+++ spec/remote_spec.rb
@@ -1,8 +1,23 @@
 require 'spec_helper'
 
 describe GH::Remote do
-  it 'loads resources from github'
-  it 'sets headers correctly'
-  it 'raises an exception for missing resources'
-  it 'parses the body'
+  it 'loads resources from github' do
+    stub_request(:get, "https://api.github.com/foo").to_return(:body => '"foo"')
+    subject['foo'].to_s.should be == '"foo"'
+  end
+
+  it 'sets headers correctly' do
+    stub_request(:get, "https://api.github.com/foo").to_return(:headers => {'X-Foo' => 'bar'})
+    subject['foo'].headers['x-foo'].should be == 'bar'
+  end
+
+  it 'raises an exception for missing resources' do
+    stub_request(:get, "https://api.github.com/foo").to_return(:status => 404)
+    expect { subject['foo'] }.to raise_error(Faraday::Error::ResourceNotFound)
+  end
+
+  it 'parses the body' do
+    stub_request(:get, "https://api.github.com/foo").to_return(:body => '{"foo":"bar"}')
+    subject['foo']['foo'].should be == 'bar'
+  end
 end
