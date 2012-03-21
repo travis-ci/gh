2b463c96902c3cc410c68043e96cd2cca53f3d1f
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-21 18:54:29 +0100

    add scoping

--- lib/gh.rb
+++ lib/gh.rb
@@ -9,4 +9,23 @@ module GH
   autoload :Response,   'gh/response'
   autoload :Stack,      'gh/stack'
   autoload :Wrapper,    'gh/wrapper'
+
+  def self.[](key)
+    backend = Thread.current[:GH] ||= DefaultStack.build
+    backend[key]
+  end
+
+  def self.with(backend)
+    backend = DefaultStack.build(backend) if Hash === backend
+    was, Thread.current[:GH] = Thread.current[:GH], backend
+    yield
+  ensure
+    Thread.current[:GH] = was
+  end
+
+  DefaultStack = Stack.new do
+    use Cache
+    use Normalizer
+    use Remote
+  end
 end
