f4a1ad5c48343bbe41a096dd804f4349e819d783
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-03-21 19:20:04 +0100

    also check for RAILS_CACHE

--- lib/gh/cache.rb
+++ lib/gh/cache.rb
@@ -27,7 +27,7 @@ module GH
 
     # Internal: Initializes a new Cache instance.
     def setup(*)
-      self.cache ||= Rails.cache if defined? Rails.cache
+      self.cache ||= Rails.cache if defined? Rails.cache and defined? RAILS_CACHE
       self.cache ||= ActiveSupport::Cache.lookup_store if defined? ActiveSupport::Cache.lookup_store
       self.cache ||= SimpleCache.new
       super
