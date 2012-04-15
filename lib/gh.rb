d59d0aa70c0f30300325721ae352091add19dcec
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-04-15 13:54:58 +0200

    add response wrapper

--- lib/gh.rb
+++ lib/gh.rb
@@ -3,16 +3,17 @@ require 'backports'
 require 'forwardable'
 
 module GH
-  autoload :Cache,        'gh/cache'
-  autoload :Case,         'gh/case'
-  autoload :LazyLoader,   'gh/lazy_loader'
-  autoload :LinkFollower, 'gh/link_follower'
-  autoload :MergeCommit,  'gh/merge_commit'
-  autoload :Normalizer,   'gh/normalizer'
-  autoload :Remote,       'gh/remote'
-  autoload :Response,     'gh/response'
-  autoload :Stack,        'gh/stack'
-  autoload :Wrapper,      'gh/wrapper'
+  autoload :Cache,            'gh/cache'
+  autoload :Case,             'gh/case'
+  autoload :LazyLoader,       'gh/lazy_loader'
+  autoload :LinkFollower,     'gh/link_follower'
+  autoload :MergeCommit,      'gh/merge_commit'
+  autoload :Normalizer,       'gh/normalizer'
+  autoload :Remote,           'gh/remote'
+  autoload :Response,         'gh/response'
+  autoload :ResponseWrapper,  'gh/response_wrapper'
+  autoload :Stack,            'gh/stack'
+  autoload :Wrapper,          'gh/wrapper'
 
   def self.with(backend)
     backend = DefaultStack.build(backend) if Hash === backend
