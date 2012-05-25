91ba12163bc8801eaf10551aa62dd4393e9846e2
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-05-25 20:32:24 +0200

    add missing file

---
+++ lib/gh/faraday_adapter.rb
@@ -0,0 +1,51 @@
+require 'gh'
+require 'faraday'
+require 'net/https'
+require 'net/http/pipeline'
+require 'net/http/persistent'
+
+module GH
+  # Faraday adapter based on Net::HTTP, with persistent connection and pipeline support.
+  class FaradayAdapter < Faraday::Adapter::NetHttpPersistent
+    class Manager
+      def initialize(*)
+        @requests = {}
+        @connects = {}
+      end
+
+      def add_request(http, env, adapter)
+        url = env[:url] + '/'
+        env[:adapter] = adapter
+        @requests[url] ||= []
+        @requests[url] << env
+      end
+
+      def run
+        http = Net::HTTP::Persistent.new 'GH'
+        @requests.each do |url, envs|
+          requests  = envs.map { |env| env[:adapter].create_request(env) }
+          responses = http.pipeline(url, requests)
+          envs.zip(responses) do |e,r|
+            e[:adapter].save_response(e, r.code.to_i, r.body) { |h| r.each_header { |k,v| h[k] = v } }
+          end
+        end
+      end
+    end
+
+    self.supports_parallel = true
+
+    def self.setup_parallel_manager(options = {})
+      Manager.new(options)
+    end
+
+    def call(env)
+      catch(:parallel) { super }
+    end
+
+    def perform_request(http, env)
+      return super unless env[:parallel_manager]
+      env[:parallel_manager].add_request(http, env, self)
+      throw :parallel, @app.call(env)
+    end
+  end
+end
