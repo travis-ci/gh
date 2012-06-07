253b3d9346a2d384ed31d31bb952e0b7cf2067cb
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-06-07 15:50:36 +0200

    better errors

--- lib/gh/error.rb
+++ lib/gh/error.rb
@@ -4,6 +4,10 @@ module GH
   class Error < Exception
     attr_reader :error, :payload
 
+    def self.new(error, *)
+      Error === error ? error : super
+    end
+
     def initialize(error = nil, payload = nil)
       error = error.error while error.respond_to? :error
       @error, @payload = error, payload
@@ -11,7 +15,13 @@ module GH
     end
 
     def message
-      "GH request failed (#{error.message}) with payload: #{payload.inspect}"
+      "GH request failed (#{error.class}: #{error.message}) with payload: #{short(payload.inspect)}"
+    end
+
+    private
+
+    def short(string)
+      string.length < 101 ? string : string[0,97] + '...'
     end
   end
 end
