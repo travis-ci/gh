ac77e03b8fb70bec5cc4d218fe99d9348f614de9
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-04-22 14:43:23 +0200

    fix token check

--- lib/gh/token_check.rb
+++ lib/gh/token_check.rb
@@ -1,19 +1,22 @@
 require 'gh/error'
+require 'base64'
 
 module GH
   class TokenCheck < Wrapper
-    attr_accessor :client_id, :token
+    attr_accessor :client_id, :client_secret, :token
 
     def setup(backend, options)
-      @client_id   = options[:client_id]
-      @token       = options[:token]
-      @check_token = true
+      @client_secret = options[:client_secret]
+      @client_id     = options[:client_id]
+      @token         = options[:token]
+      @check_token   = true
       super
     end
 
     def check_token
-      return unless @check_token and client_id and token
-      http :head, "/applications/#{client_id}/tokens/#{token}"
+      return unless @check_token and client_id and client_secret and token
+      auth_header = "Basic %s" % Base64.encode64("#{client_id}:#{client_secret}").gsub("\n")
+      http :head, "/applications/#{client_id}/tokens/#{token}", "Authorization" => auth_header
       @check_token = false
     rescue GH::Error
       raise GH::TokenInvalid
