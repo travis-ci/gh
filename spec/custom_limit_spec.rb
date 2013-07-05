7c2ac042edfa9c27717344108929f2175e2126da
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2013-07-06 00:06:32 +0200

    make specs version independent

--- spec/custom_limit_spec.rb
+++ spec/custom_limit_spec.rb
@@ -8,7 +8,7 @@ describe GH::CustomLimit do
 
   it 'adds client_id and client_secret to a request' do
     headers = {
-      "User-Agent"     => "GH/0.11.2",
+      "User-Agent"     => "GH/#{GH::VERSION}",
       "Accept"         => "application/vnd.github.v3+json,application/vnd.github.beta+json;q=0.5,application/json;q=0.1",
       "Accept-Charset" => "utf-8"
     }
