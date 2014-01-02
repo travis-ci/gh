d850b2c4c049241480dfeb86c07b1ba24d0e608f
Mathias Meyer <meyer@paperplanes.de>
2014-01-02 16:02:37 +0100

    Fix shell syntax. I have no idea how shell works.

--- fix-bundler.sh
+++ fix-bundler.sh
@@ -4,7 +4,7 @@ set -e
 
 RUBY_VERSION=`ruby -v`
 
-if [ "echo ${RUBY_VERSION} | grep '1\.8\.7' 2>&1 > /dev/null" ]
+if echo ${RUBY_VERSION} | grep '1\.8\.7' 2>&1 > /dev/null
 then
   echo 'Updating RubyGems to 2.1.11'
   gem update --system 2.1.11
@@ -12,8 +12,8 @@ fi
 
 BUNDLER_VERSION=`bundle -v`
 
-if [ "echo ${BUNDLER_VERSION} | grep '1\.5\.0' 2>&1 > dev/null" ]
+if echo ${BUNDLER_VERSION} | grep '1\.5\.0' 2>&1 > dev/null
 then
-  puts 'Updating Bundler to 1.5.1'
+  echo 'Updating Bundler to 1.5.1'
   gem install bundler -v '= 1.5.1'
 fi
