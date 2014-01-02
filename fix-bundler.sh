80afbb0e3937497a3b537900d2d0aa1e98109fd1
Mathias Meyer <meyer@paperplanes.de>
2014-01-02 16:05:05 +0100

    It's /dev/null

--- fix-bundler.sh
+++ fix-bundler.sh
@@ -12,7 +12,7 @@ fi
 
 BUNDLER_VERSION=`bundle -v`
 
-if echo ${BUNDLER_VERSION} | grep '1\.5\.0' 2>&1 > dev/null
+if echo ${BUNDLER_VERSION} | grep '1\.5\.0' 2>&1 > /dev/null
 then
   echo 'Updating Bundler to 1.5.1'
   gem install bundler -v '= 1.5.1'
