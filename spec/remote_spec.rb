3009109ab4ab2d711ad4b7e9337c39c6265caa60
David Calavera <david.calavera@gmail.com>
2015-02-24 09:22:12 -0800

    Support old rubies.

--- spec/remote_spec.rb
+++ spec/remote_spec.rb
@@ -40,7 +40,7 @@ describe GH::Remote do
 
   it 'loads resources from github' do
     stub_request(:get, "https://api.github.com/foo")
-      .with(headers: {"Accept" => "application/vnd.github.v3+json,application/json"})
+      .with(:headers => {"Accept" => "application/vnd.github.v3+json,application/json"})
       .to_return(:body => '["foo"]')
 
     GH::Remote.new(accept: "application/vnd.github.v3+json,application/json")['foo'].to_s.should be == '["foo"]'
