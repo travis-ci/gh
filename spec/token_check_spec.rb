48896d32e8f1f73a5c88b5fc422285030b7cd816
Hiro Asari <asari.ruby@gmail.com>
2020-03-31 19:02:53 -0400

    Fix specs

--- spec/token_check_spec.rb
+++ spec/token_check_spec.rb
@@ -8,7 +8,7 @@ describe GH::TokenCheck do
   end
 
   it 'adds client_id and client_secret to a request' do
-    subject.backend.should_receive(:http).with(:post, "/applications/foo/token?access_token=baz", "Authorization" => "Basic Zm9vOmJhcg==") do
+    expect(subject.backend).to receive(:http).with(:post, "/applications/foo/token", :body => "{\"access_token\": \"baz\"}", "Authorization" => "Basic Zm9vOmJhcg==") do
       error = GH::Error.new
       error.info[:response_status] = 404
       raise error
@@ -17,12 +17,11 @@ describe GH::TokenCheck do
   end
 
   it 'does not swallow other status codes' do
-    pending "test needs rewrite for newer RSpec"
-    subject.backend.should_receive(:http).with(:post, "/applications/foo/token?access_token=baz", "Authorization" => "Basic Zm9vOmJhcg==") do
+    expect(subject.backend).to receive(:http).with(:post, "/applications/foo/token", :body => "{\"access_token\": \"baz\"}", "Authorization" => "Basic Zm9vOmJhcg==") do
       error = GH::Error.new
       error.info[:response_status] = 500
       raise error
     end
-    expect { subject['/x'] }.not_to raise_error
+    expect { subject['/x'] }.to raise_error(GH::Error(:response_status => 500))
   end
 end
